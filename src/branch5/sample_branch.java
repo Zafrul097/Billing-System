2c48c44843ab956f145d7ce1ab08b3f13bea7afb
Zafrul097 <zafrulislamridoy@gmail.com>
2022-05-06 03:38:25 +0600

    I have made some changes in code here

--- src/branch5/sample_branch.java
+++ src/branch5/sample_branch.java
@@ -5,6 +5,29 @@ public class sample_branch {
     float cgpa;
     String name,address;
 
+    string car_no,blood_group;
+
+    public sample_branch(string car_no, string blood_group) {
+        this.car_no = car_no;
+        this.blood_group = blood_group;
+    }
+
+    public string getCar_no() {
+        return car_no;
+    }
+
+    public void setCar_no(string car_no) {
+        this.car_no = car_no;
+    }
+
+    public string getBlood_group() {
+        return blood_group;
+    }
+
+    public void setBlood_group(string blood_group) {
+        this.blood_group = blood_group;
+    }
+
     public sample_branch(int id, float cgpa, String name, String address) {
         this.id = id;
         this.cgpa = cgpa;
