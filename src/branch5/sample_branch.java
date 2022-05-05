b3ee88745c206805d37137e5933b4ffed573cda5
Zafrul097 <zafrulislamridoy@gmail.com>
2022-05-06 03:23:12 +0600

    java class added in branch

---
+++ src/branch5/sample_branch.java
@@ -0,0 +1,46 @@
+package branch5;
+
+public class sample_branch {
+    int id;
+    float cgpa;
+    String name,address;
+
+    public sample_branch(int id, float cgpa, String name, String address) {
+        this.id = id;
+        this.cgpa = cgpa;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public float getCgpa() {
+        return cgpa;
+    }
+
+    public void setCgpa(float cgpa) {
+        this.cgpa = cgpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
